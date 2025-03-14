a2e2fe82f53f08c7b9742e73247f9688e9a20992
Bozhidar Videnov <bozh.bv03@gmail.com>
2025-03-14 22:43:44 +0200

    feat(server): added fiber

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+
+	"github.com/MartinM2304/hackathon2025/internal/web"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	app := fiber.New()
+
+	app.Use(logger.New(logger.Config{
+		Format:     "${pid} ${time} ${status} - ${method} ${path}\n",
+		TimeFormat: "02-01-2006",
+		TimeZone:   "Europe/Sofia",
+	}))
+
+	router := app.Group("/api")
+	web.Register(router)
+
+	err := app.Listen(":3000")
+	if err != nil {
+		slog.Error("Error starting the server %s", err.Error())
+	}
 }
---
+++ server/internal/web/routes.go
@@ -0,0 +1,14 @@
+package web
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func Register(router fiber.Router) {
+	router.Use(cors.New())
+
+	router.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Api!")
+	})
+}
