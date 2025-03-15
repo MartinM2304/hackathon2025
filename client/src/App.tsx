92147dc88cad409a865bdf72a63f2f6e20f98d5c
MartinM2304 <mmarinkov6@gmail.com>
2025-03-15 15:57:51 +0200

    issue with cors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import DirectionButtons from './mainView/DirectionButtons';
 import EmojiButtons from './mainView/EmojiButtons';
+import LogDisplay from './mainView/LogDisplay';
 import SoundList from './mainView/SoundList'
 import { VideoPlayer } from './VidePlayer';
 
@@ -19,7 +20,8 @@ function App() {
             </div>
           </div>
         </div> */}
-
+        {/* Log Display */}
+          <LogDisplay socketUrl="http://localhost:3000" />
         {/* Actions Section */}
         <div className="space-y-4">
           <div>
--- client/src/mainView/DirectionButton.tsx
+++ client/src/mainView/DirectionButton.tsx
@@ -20,7 +20,8 @@ const DirectionButton: React.FC<DirectionButtonProps> = ({
 
   const handleDirectionClick = async (direction: number) => {
     try {
-      const response = await fetch("http://localhost:3000/api/direction", {
+      const apiUrl = import.meta.env.VITE_API_URL;
+      const response = await fetch("${apiUrl}/api/direction", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
