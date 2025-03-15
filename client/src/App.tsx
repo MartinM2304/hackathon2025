86f92c4999da2051b1f197f8498540765d26cb48
Alexander Asenov <alexspx@hotmail.com>
2025-03-15 22:56:54 +0200

    sound style fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,16 +9,14 @@ function App() {
     <div className="flex flex-col md:flex-row h-screen bg-gray-900 text-white">
       <VideoPlayer src='https://robot.g8row.xyz/hls/robot.m3u8' />
     
-      <div className="w-full md:w-96 p-8 bg-gray-800 h-full p-3">
-      <div className="flex flex-col gap-6 w-full">
+      <div className="flex flex-col w-full gap-6 md:w-96 p-8 bg-gray-800 h-full p-3">
         {/* Log Display */}
           <LogDisplay socketUrl={"https://server.g8row.xyz/socket.io"} />
         {/* Actions Section */}
 
-            <DirectionButtons />
-            <EmojiButtons />
-            <SoundList />
-        </div>
+          <DirectionButtons />
+          <EmojiButtons />
+          <SoundList />
       </div>
     </div>
   );
