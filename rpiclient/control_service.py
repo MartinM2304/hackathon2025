44d70d7a10f68650d8074d941c51fc1544e380e5
g8row <g8row@github.com>
2025-03-16 04:05:54 +0000

    add nginx configs and finish up control script

--- rpiclient/control_service.py
+++ rpiclient/control_service.py
@@ -1,20 +1,19 @@
-# control_service.py (runs alongside FFmpeg)
 import requests
 import serial
 import time
 import threading
 from playsound import playsound
 
-# Configuration
 API_URL = "https://server.g8row.xyz/api/data"
 ARDUINO_PORT = "/dev/ttyUSB0"
 POLL_INTERVAL = 5  # Seconds
 newline = "\n"
 sounds = ["bark","meow","hello","bye"]
 
+
 class RobotController:
     def __init__(self):
-#        self.ser = serial.Serial(ARDUINO_PORT, 115200, timeout=1)
+        self.ser = serial.Serial(ARDUINO_PORT, 115200, timeout=1)
         self.last_command = None
         
     def fetch_command(self):
@@ -31,10 +30,10 @@ class RobotController:
 
     def execute_command(self, direction, emoji, sound):
             if direction != None:
- #               self.ser.write(str(direction["Id"]).encode()+newline.encode())    
+                self.ser.write(str(direction["Id"]).encode()+newline.encode())    
                 print(f"{direction}" )
             if emoji != None:
-  #              self.ser.write(str(emoji["Id"]).encode()+newline.encode())
+                self.ser.write(str(emoji["Id"]).encode()+newline.encode())
                 print(f"{emoji}" )
             if sound != None:
                 playsound(f"/home/pi/hackathon2025/rpiclient/{sounds[sound['Id']]}.mp3")        
@@ -49,15 +48,12 @@ def control_loop():
         
         controller.execute_command(command[0],command[1],command[2])
                 
-        # Dynamic interval adjustment
         elapsed = time.monotonic() - start_time
         sleep_time = max(0, POLL_INTERVAL - elapsed)
         time.sleep(sleep_time)
 
 if __name__ == "__main__":
-    # Start in separate thread to keep responsive
     threading.Thread(target=control_loop, daemon=True).start()
     
-    # Monitor system health
     while True:
         time.sleep(60)
